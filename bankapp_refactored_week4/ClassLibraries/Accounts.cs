30ad8d42f0ff0f5ab364b9486d889ae756fd39d5
Fred-Michael <chinazorfredrick@gmail.com>
2020-08-08 16:17:43 +0100

    implementing some functions in accountClass and creating the customerClass

--- bankapp_refactored_week4/ClassLibraries/Accounts.cs
+++ bankapp_refactored_week4/ClassLibraries/Accounts.cs
@@ -6,5 +6,35 @@ namespace bankapp_refactored_week4.ClassLibraries
 {
     public class Accounts
     {
+        private int accountNumberGenerator = 100001;
+        public AccountTypes AccountType { get; private set; }
+        public string AccountNumber { get; private set; }
+        public DateTime AccountDateCreation { get; set; }
+        public string AccountOwnerID { get; set; }
+        public int Balance { get; set; }
+        public List<Transaction> AccountTransactions { get; set; }
+
+        public Accounts(string ownerID, int typeOfAccount, decimal firstDeposit)
+        {
+            AccountNumber = accountNumberGenerator.ToString();
+            accountNumberGenerator++;
+            AccountOwnerID = ownerID;
+            AccountType = (AccountTypes)typeOfAccount;
+            AccountDateCreation = DateTime.Now;
+        }
+
+        public void MakeDeposit()
+        {
+            //
+        }
+        public void MakeWithdrawal()
+        {
+            //
+        }
+        public void MakeTransfer()
+        {
+            //
+        }
     }
+
 }
--- bankapp_refactored_week4/ClassLibraries/Customer.cs
+++ bankapp_refactored_week4/ClassLibraries/Customer.cs
@@ -6,5 +6,19 @@ namespace bankapp_refactored_week4.ClassLibraries
 {
     public class Customer
     {
+        private int customerIDGenerator = 1;
+        public string customerName { get; set; }
+        public string customerID { get; set; }
+        public string customerEmail { get; set; }
+
+        public List<Accounts> customersAccounts { get; set; }
+
+        public Customer(string name, string email)
+        {
+            customerEmail = email;
+            customerID = customerIDGenerator.ToString();
+            customerIDGenerator++;
+            customerName = name;
+        }
     }
 }
