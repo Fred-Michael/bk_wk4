1185ec0d5b39abdd8a59a86efca59142a20ee04e
Fred-Michael <chinazorfredrick@gmail.com>
2020-08-08 10:28:23 +0100

    added comments to the BankData class

--- bankapp_refactored_week4/BankStore/BankData.cs
+++ bankapp_refactored_week4/BankStore/BankData.cs
@@ -5,6 +5,7 @@ namespace bankapp_refactored_week4.BankStore
 {
     public static class BankData
     {
+        //This BankData class holds the lists of Accounts, Customers and Transactions
         public static List<Accounts> Accounts { get; set; } = new List<Accounts>();
         public static List<Customer> Customers { get; set; } = new List<Customer>();
         public static List<Transaction> Transactions { get; set; } = new List<Transaction>();
